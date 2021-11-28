47474bf57b385abe4bd8ac62569de86c2c960021
dominikzeglen <flesz3@o2.pl>
2021-11-28 13:39:36 +0100

    Move loader to entity module

--- src/core/core.ts
+++ src/core/core.ts
@@ -1,64 +1,10 @@
 import { Cutie, CutieSimInput, getRandomCutie } from "./entities/cutie";
-import { Egg } from "./entities/egg";
 import { Food } from "./entities/food";
 import { getRandomPositionInBounds, len, Point, sub, toPolar } from "./r2";
 import { Waste } from "./entities/waste";
 import { Entity } from "./entities/entity";
 import minBy from "lodash/minBy";
-
-class EntityLoader {
-  entities: Entity[];
-
-  _food: Food[] | null;
-  _cuties: Cutie[] | null;
-  _eggs: Egg[] | null;
-  _waste: Waste[] | null;
-
-  constructor() {
-    this.entities = [];
-  }
-
-  init = (entities: Entity[]) => {
-    this.entities = entities;
-
-    this._food = null;
-    this._cuties = null;
-    this._eggs = null;
-    this._waste = null;
-  };
-
-  get food(): Food[] {
-    if (this._food) {
-      return this._food;
-    }
-
-    return this.entities.filter((entity) => entity instanceof Food);
-  }
-
-  get cuties(): Cutie[] {
-    if (this._cuties) {
-      return this._cuties;
-    }
-
-    return this.entities.filter((entity) => entity instanceof Cutie) as Cutie[];
-  }
-
-  get eggs(): Egg[] {
-    if (this._eggs) {
-      return this._eggs;
-    }
-
-    return this.entities.filter((entity) => entity instanceof Egg) as Egg[];
-  }
-
-  get waste(): Waste[] {
-    if (this._waste) {
-      return this._waste;
-    }
-
-    return this.entities.filter((entity) => entity instanceof Waste) as Waste[];
-  }
-}
+import { EntityLoader } from "./entities/loader";
 
 export class Sim {
   bounds: Point[];
@@ -96,6 +42,7 @@ export class Sim {
       stats: {
         itPerSecond: 0,
       },
+      selected: undefined,
     };
   }
 
---
+++ src/core/entities/loader.ts
@@ -0,0 +1,59 @@
+import { Cutie } from "./cutie";
+import { Egg } from "./egg";
+import { Entity } from "./entity";
+import { Food } from "./food";
+import { Waste } from "./waste";
+
+export class EntityLoader {
+  entities: Entity[];
+
+  _food: Food[] | null;
+  _cuties: Cutie[] | null;
+  _eggs: Egg[] | null;
+  _waste: Waste[] | null;
+
+  constructor() {
+    this.entities = [];
+  }
+
+  init = (entities: Entity[]) => {
+    this.entities = entities;
+
+    this._food = null;
+    this._cuties = null;
+    this._eggs = null;
+    this._waste = null;
+  };
+
+  get food(): Food[] {
+    if (this._food) {
+      return this._food;
+    }
+
+    return this.entities.filter((entity) => entity instanceof Food);
+  }
+
+  get cuties(): Cutie[] {
+    if (this._cuties) {
+      return this._cuties;
+    }
+
+    return this.entities.filter((entity) => entity instanceof Cutie) as Cutie[];
+  }
+
+  get eggs(): Egg[] {
+    if (this._eggs) {
+      return this._eggs;
+    }
+
+    return this.entities.filter((entity) => entity instanceof Egg) as Egg[];
+  }
+
+  get waste(): Waste[] {
+    if (this._waste) {
+      return this._waste;
+    }
+
+    return this.entities.filter((entity) => entity instanceof Waste) as Waste[];
+  }
+}
