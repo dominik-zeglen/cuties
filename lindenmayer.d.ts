e1a323f086706fb816bc3d706cd8b9e19a75248e
dominikzeglen <flesz3@o2.pl>
2021-12-03 15:25:38 +0100

    Fix typing

--- lindenmayer.d.ts
+++ lindenmayer.d.ts
@@ -1,15 +1,26 @@
+/* eslint-disable no-unused-vars */
+
 declare module "lindenmayer" {
   export interface Production {
     symbol: string;
   }
 
+  export interface Successor {
+    weight: number;
+    successor: string;
+  }
+
+  export interface Successors {
+    successors: Successor[];
+  }
+
   export interface LSystemInit<T extends {} = {}> {
     axiom: string;
-    productions: Record<string, string | Array<Production & T>>;
+    productions: Record<string, string | Array<Production & T> | Successors>;
   }
   export default class LSystem {
     constructor(opts: LSystemInit);
 
-    iterate: (its: number) => void;
+    iterate: (its?: number) => string;
   }
 }
