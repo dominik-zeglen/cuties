2124f920f7460b19074da93872a5f05158b103a9
dominikzeglen <flesz3@o2.pl>
2021-12-17 11:44:49 +0100

    Add proper biases

--- src/core/ai.ts
+++ src/core/ai.ts
@@ -1,4 +1,5 @@
-import { add, multiply, map, clone } from "mathjs";
+import { add, multiply, map } from "mathjs";
+import cloneDeep from "lodash/cloneDeep";
 
 type Matrix = number[][];
 
@@ -10,42 +11,62 @@ const inputs = 4;
 const outputs = 4;
 const hidden = 8;
 export type CutieOutput = Record<"speed" | "angle" | "eat" | "layEgg", number>;
-export type CutieAi = Matrix[];
+export interface CutieAiLayer {
+  biases: Matrix;
+  weights: Matrix;
+}
+export type CutieAi = CutieAiLayer[];
 
 const baseSystem: CutieAi = [
-  Array(inputs + 1)
-    .fill(0)
-    .map(() => Array(hidden).fill(0)),
-  Array(hidden + 1)
-    .fill(0)
-    .map(() => Array(outputs).fill(0)),
+  {
+    biases: Array(1)
+      .fill(0)
+      .map(() => Array(hidden).fill(0)),
+    weights: Array(inputs)
+      .fill(0)
+      .map(() => Array(hidden).fill(0)),
+  },
+  {
+    biases: Array(1)
+      .fill(0)
+      .map(() => Array(outputs).fill(0)),
+    weights: Array(hidden)
+      .fill(0)
+      .map(() => Array(outputs).fill(0)),
+  },
 ];
 
-function getRandomSystem(divide: number): Matrix[] {
-  const system = clone(baseSystem);
+function getRandomSystem(divide: number): CutieAi {
+  const system = cloneDeep(baseSystem);
 
   for (let layer = 0; layer < system.length; layer++) {
     const mutations = Math.random() * inputs + 1;
 
     for (let index = 0; index < mutations; index++) {
-      system[layer][Math.floor(Math.random() * inputs)][
-        Math.floor(Math.random() * inputs)
-      ] = (Math.random() > 0.5 ? 1 : -1) / divide;
+      const xb = Math.floor(Math.random() * system[layer].biases.length);
+      const yb = Math.floor(Math.random() * system[layer].biases[xb].length);
+      system[layer].biases[xb][yb] = (Math.random() > 0.5 ? 1 : -1) / divide;
+
+      const xw = Math.floor(Math.random() * system[layer].biases.length);
+      const yw = Math.floor(Math.random() * system[layer].biases[xw].length);
+      system[layer].weights[xw][yw] = (Math.random() > 0.5 ? 1 : -1) / divide;
     }
   }
 
   return system;
 }
 
-function addSystems(a: Matrix[], b: Matrix[]): Matrix[] {
-  return a.map((_, layerIndex) => add(a[layerIndex], b[layerIndex]) as Matrix);
+function addSystems(a: CutieAi, b: CutieAi): CutieAi {
+  return a.map((_, layerIndex) => ({
+    biases: add(a[layerIndex].biases, b[layerIndex].biases) as Matrix,
+    weights: add(a[layerIndex].weights, b[layerIndex].weights) as Matrix,
+  }));
 }
 
 export function getRandomCutieAi(): CutieAi {
   return addSystems(baseSystem, getRandomSystem(1));
 }
 
-// 1 x inputs + 1 . inputs + 1 x hidden + 1 . hidden + 1 x outputs => 1 x outputs
 export function think(input: CutieInput, ai: CutieAi): CutieOutput {
   const inputMatrix = [
     [input.angle, input.angleToFood, input.distanceToFood, input.hunger],
@@ -54,20 +75,17 @@ export function think(input: CutieInput, ai: CutieAi): CutieOutput {
   const output = ai.reduce(
     (prevLayer, layer) =>
       map(
-        multiply(
-          prevLayer.map((row) => [...row, 1]),
-          layer
-        ),
+        add(multiply(prevLayer, layer.weights), layer.biases) as Matrix,
         Math.tanh
       ),
     inputMatrix
-  );
+  )[0];
 
   return {
-    angle: output[0][0],
-    speed: output[0][1],
-    eat: output[0][2],
-    layEgg: output[0][3],
+    angle: output[0],
+    speed: output[1],
+    eat: output[2],
+    layEgg: output[3],
   };
 }
 
