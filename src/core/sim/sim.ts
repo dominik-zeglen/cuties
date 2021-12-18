406648250429a9ab433011798806e5952c721a11
dominikzeglen <flesz3@o2.pl>
2021-12-18 21:29:52 +0100

    Modularize code

--- src/core/sim/sim.ts
+++ src/core/sim/sim.ts
@@ -1,50 +1,56 @@
 import type { Sim } from ".";
-import { CutieSimInput, rangeRadius } from "../entities/cutie";
+import { Cutie, CutieSimInput, rangeRadius } from "../entities/cutie";
 import { Flower, rangeRadius as flowerRangeRadius } from "../entities/flowers";
 import { Waste } from "../entities/waste";
 import { sub, toPolar } from "../r2";
 
+export function getCutieInput(cutie: Cutie, sim: Sim): CutieSimInput {
+  let simInput: CutieSimInput | null = null;
+  const nearestFood = sim.getNearestFood(cutie.position, rangeRadius);
+  if (nearestFood) {
+    const nearestFoodPolarPosition = toPolar(
+      sub(cutie.position, nearestFood.position)
+    );
+
+    if (nearestFoodPolarPosition.r < 10 && cutie.wantsToEat()) {
+      cutie.eat(nearestFood);
+    }
+    simInput = {
+      iteration: sim.iteration,
+      nearestFood: nearestFoodPolarPosition,
+    };
+  } else {
+    simInput = {
+      iteration: sim.iteration,
+      nearestFood: { angle: 0, r: 0 },
+    };
+  }
+
+  return simInput;
+}
+
+export function simCutie(cutie: Cutie, sim: Sim): void {
+  cutie.sim(getCutieInput(cutie, sim));
+  if (cutie.wantsToLayEgg() && cutie.canLayEgg()) {
+    sim.registerEntity(cutie.layEgg(sim.iteration));
+  }
+
+  if (cutie.shouldDumpWaste()) {
+    const waste = cutie.dumpWaste();
+    sim.registerEntity(waste);
+  }
+
+  if (cutie.shouldDelete) {
+    const remains = new Waste({
+      position: cutie.position,
+    });
+    remains.value = 100;
+    sim.registerEntity(remains);
+  }
+}
+
 export function simCuties(sim: Sim) {
-  sim.entityLoader.cuties.forEach((cutie) => {
-    let simInput: CutieSimInput | null = null;
-    const nearestFood = sim.getNearestFood(cutie.position, rangeRadius);
-    if (nearestFood) {
-      const nearestFoodPolarPosition = toPolar(
-        sub(cutie.position, nearestFood.position)
-      );
-
-      if (nearestFoodPolarPosition.r < 10 && cutie.wantsToEat()) {
-        cutie.eat(nearestFood);
-      }
-      simInput = {
-        iteration: sim.iteration,
-        nearestFood: nearestFoodPolarPosition,
-      };
-    } else {
-      simInput = {
-        iteration: sim.iteration,
-        nearestFood: { angle: 0, r: 0 },
-      };
-    }
-
-    cutie.sim(simInput);
-    if (cutie.wantsToLayEgg() && cutie.canLayEgg()) {
-      sim.registerEntity(cutie.layEgg(sim.iteration));
-    }
-
-    if (cutie.shouldDumpWaste()) {
-      const waste = cutie.dumpWaste();
-      sim.registerEntity(waste);
-    }
-
-    if (cutie.shouldDelete) {
-      const remains = new Waste({
-        position: cutie.position,
-      });
-      remains.value = 100;
-      sim.registerEntity(remains);
-    }
-  });
+  sim.entityLoader.cuties.forEach((cutie) => simCutie(cutie, sim));
 }
 
 function simFlower(flower: Flower, sim: Sim) {
